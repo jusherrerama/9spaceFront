e748c2163a06905234e8a6b2effac955dca15361
Unknown <jusherrerama@unal.edu.co>
2017-10-16 22:56:35 -0500

    comment

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -5,8 +5,9 @@ import { VideoProvider } from '../../providers/video/video';
 import { UserProvider } from '../../providers/user/user';
 import { Storage } from '@ionic/storage';
 import { MyVideosPage }from '../my-videos/my-videos';
-
+import { CommentsProvider } from '../../providers/comments/comments';
 import { LikesPage }from '../likes/likes';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 /**
  * Generated class for the VideoPage page.
  *
@@ -21,14 +22,23 @@ import { LikesPage }from '../likes/likes';
 })
 export class VideoPage {
   videos: any;
+  comments: any;
   video_id: any;
   user_id: any;
   user: any;
   user_id_to_see: any;
   profile: any;
-  constructor(public modalCtrl: ModalController,private storage: Storage,public navCtrl: NavController, public navParams: NavParams,
-     public videoProvider: VideoProvider, public userProvider: UserProvider,public actionSheetCtrl: ActionSheetController) {
-
+  newComment = { text: '', user_id: '', video_id: '' };
+  responseData: any;
+    myForm: FormGroup;
+  constructor(public commentsProvider: CommentsProvider,public formBuilder: FormBuilder,
+    public modalCtrl: ModalController,private storage: Storage,public navCtrl: NavController,
+     public navParams: NavParams,
+     public videoProvider: VideoProvider, public userProvider: UserProvider,
+     public actionSheetCtrl: ActionSheetController) {
+       this.myForm = this.formBuilder.group({
+           text: ['', Validators.required]
+         });
        storage.get('user_id').then((val) => {
            console.log("acacaca"+val)
 
@@ -39,14 +49,17 @@ export class VideoPage {
 
          this.user_id_to_see = this.navParams.get('param1');
 
-
+         this.newComment.user_id = this.user_id_to_see
         this.addMyVideosR();
         this.getVideo();
+
   }
   presentModal(id) {
       console.log("aSIII ES EPERFIILL ENTRAsssssssssssssssA" + id)
      this.navCtrl.push(LikesPage,{ "param1":id});
    }
+
+
   usser(id){
       this.user_id =id;
       if (  (this.user_id ==  this.user_id_to_see) || (  this.user_id_to_see==null ) ){
@@ -96,7 +109,9 @@ export class VideoPage {
     .then(data => {
       this.videos = data;
       console.log( this.videos);
+        this.newComment.video_id = this.videos.id
         this.getuser();
+        this.getComments();
     });
 
   }
@@ -107,6 +122,26 @@ export class VideoPage {
       this.user = data;
       console.log(this.user);
     });
+  }
+  getComments() {
+
+    this.commentsProvider.getCommentsByVideo(this.videos.id)
+    .then(data => {
+      this.comments = data;
+      console.log(this.comments);
+    });
+
+  }
+  addComment(){
+    console.log(this.newComment);
+
+  this.commentsProvider.addComment(this.newComment)
+        .then(data => {
+          this.responseData = data;
+          console.log(this.responseData);
+
+        });
+    this.navCtrl.setRoot(VideoPage,{ "param1":this.videos.id});
   }
   goToUser(id){
 
